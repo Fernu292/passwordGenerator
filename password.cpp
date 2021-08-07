86f2632f4e6b1d0d2fa46744667d642249eeff81
Fernu292 <fernu292@gmail.com>
2021-08-06 21:05:44 -0500

    Create password.cpp

---
+++ password.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+
+
+using namespace std;
+
+static const char alphanum[] =
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+"abcdefghijklmnopqrstuvwxyz"
+"0123456789"
+"!@#$%^&*";
+
+
+int size = sizeof(alphanum) - 1;
+
+
+int main()
+{
+	int length;
+
+    cout<<"Select your length password: ";
+    cin>>length;
+	
+	srand(time(0));
+	for (int i = 0; i < length; i++)
+	{
+		cout << alphanum[rand() % ::size];
+	}
+	return 0;
+}
