660e351c11d09b9f66d4d66e91cfde2bd7d7e65b
Fernu292 <fernu292@gmail.com>
2021-08-18 14:23:14 -0500

    JavaScrpt Version
    Password Generator in JS

--- JavaScript/paswordJS.js
+++ JavaScript/paswordJS.js
@@ -1,6 +1,23 @@
 
-    console.log('Digite el tamaño de su contraseña: ');
-    const length = readline();
+const prompt = require('prompt-sync')();//This make an alternative to take input
 
-    console.log(length);
+
+function random(min, max){
+    return Math.floor((Math.random()*(max-min+1))+min);
+}
+
+const char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+let password = "";
+
+
+const size = char.length;
+const length = prompt('Select the length of your password: ');
+
+for(let i=0;i<length;i++){
+    password+=char[random(0,70)];
+}
+
+console.log(`Your password is: ${password}`)
+
+
 
